5c82d323f783e6446135ac9a6ae13c3547046988
WojtekK78 <20930258+WojtekK78@users.noreply.github.com>
2019-03-23 02:09:08 +0100

    Fixes to pushover notification and options to search fro multiple doctors

--- README.md
+++ README.md
@@ -56,6 +56,12 @@ Oczywiście znalezienie wizyty do endokrynologa nie jest takie proste, więc ust
 medihunter find-appointment -s 27962 -i 1
 ```
 
+a może chcemy poszukać konkretnych endokrynolgów od ID 12345 i 0987 ?
+
+```bash
+medihunter find-appointment -s 27962 -i 1 -o 12345 -o 0987
+```
+
 ## Wyświetlanie pomocy
 
 Ogólna pomoc
@@ -71,6 +77,12 @@ medihunter find-appointment --help
 medihunter show-params --help
 ```
 
+## Powiadomienia Pushover
+
+Poprzez podanie parametrów do powiadomień Pushover https://pushover.net/ możliwe jest przekazywanie powiadomień na wizytę do aplikacji Pushover.
+Należey podać minimalnie parametry --pushover_token oraz --pushover_user
+
+
 ## Domyślne ustawienia
 
 ### show-params
@@ -84,6 +96,10 @@ opcja|domyślna wartość
 -r, --region|Warszawa 
 -s, --specialization|Medicover Express - przeziębienie, grypa
 -c, --clinic|wszystkie jakie są w regionie/mieście
--o, --doctor|wszyscy lekarze
+-o, --doctor|wszyscy lekarze, można użyć wielokrotnie w celu sprawdzenie kilku lekarzy
 -d, --start-date|data bieżąca
 -i, --interval|brak
+--pushover_token|brak, Pushover Application Token
+--pushover_user|brak, Pushover user Token
+--pushover_device|brak, None nazwa device domyślnie wszystkie
+--pushover_msgtitle|brak - prefix dodawany przed tytułem powiadomienia
--- medihunter.py
+++ medihunter.py
@@ -19,13 +19,14 @@ now_formatted = now.strftime('%Y-%m-%dT02:00:00.000Z')
 @click.option('--region', '-r', default=204)
 @click.option('--specialization', '-s', default=16234)
 @click.option('--clinic', '-c', default=-1)
-@click.option('--doctor', '-o', default=-1)
+@click.option('--doctor', '-o', multiple=True, default=-1)
 @click.option('--start-date', '-d', default=now_formatted)
 @click.option('--interval', '-i', default=0)
 @click.option('--pushover_token', default="")
 @click.option('--pushover_user', default="")
 @click.option('--pushover_device', default=None)
-def find_appointment(user, password, region, specialization, clinic, doctor, start_date, interval, pushover_token, pushover_user,pushover_device):
+@click.option('--pushover_msgtitle', default="")
+def find_appointment(user, password, region, specialization, clinic, doctor, start_date, interval, pushover_token, pushover_user,pushover_device,pushover_msgtitle):
     counter = 0
     med_session = MedicoverSession(username=user, password=password)
 
@@ -50,35 +51,39 @@ def find_appointment(user, password, region, specialization, clinic, doctor, sta
     med_session.load_search_form()  # TODO: can I get rid of it?
 
     while interval > 0 or counter < 1:
-        appointments = med_session.search_appointments(
-            region=region, specialization=specialization, clinic=clinic, doctor=doctor, start_date=start_date)
+        notification = ""
+        notificationcounter = 0
+        for d in doctor:
+            appointments = med_session.search_appointments(
+                region=region, specialization=specialization, clinic=clinic, doctor=d, start_date=start_date)
 
-        if not appointments:
-            click.echo(click.style(
-                f'(iteration: {counter}) No results found', fg='yellow'))
-        else:
-            applen = len(appointments)
-            notification = ""
-            notificationcounter = 0
-            visistshelve = shelve.open('./visits.db')
-            click.echo(click.style(f'(iteration: {counter}) Found {applen} appointments', fg='green', blink=True))
-            for appointment in appointments:
-                appointmentcheck = user + appointment.appointment_datetime + appointment.doctor_name
-                click.echo(
-                    appointment.appointment_datetime + ' ' +
-                    click.style(appointment.doctor_name, fg='bright_green') + ' ' +
-                    appointment.clinic_name
-                )
-                if pushover_notification :
-                    alreadynotified = appointmentcheck in list(visistshelve.values())
-                    if not alreadynotified:
-                        notificationcounter += 1
-                        visistshelve[appointmentcheck] = appointmentcheck 
-                        notification = notification + '<b>' + appointment.appointment_datetime + '</b> <font color="#0000ff">' + appointment.doctor_name + '</font> ' + appointment.clinic_name + '\n'
-            if pushover_notification and notificationcounter > 0 :
-                if len(notification) > 1020 : notification = notification [0:960] + '<b><font color="#ff0000"> + more appointments online</font></b>'
-                client.send_message(notification, title="Found " + str(notificationcounter) + " appointments", device=pushover_device,html=1)
-            visistshelve.close()
+            if not appointments:
+                click.echo(click.style(
+                    f'(iteration: {counter}) No results found', fg='yellow'))
+            else:
+                applen = len(appointments)
+                visistshelve = shelve.open('./visits.db')
+                click.echo(click.style(f'(iteration: {counter}) Found {applen} appointments', fg='green', blink=True))
+                for appointment in appointments:
+                    appointmentcheck = user + appointment.appointment_datetime + appointment.doctor_name
+                    click.echo(
+                        appointment.appointment_datetime + ' ' +
+                        click.style(appointment.doctor_name, fg='bright_green') + ' ' +
+                        appointment.clinic_name
+                    )
+                    if pushover_notification :
+                        alreadynotified = appointmentcheck in list(visistshelve.values())
+                        if not alreadynotified:
+                            notificationcounter += 1
+                            visistshelve[appointmentcheck] = appointmentcheck 
+                            notification = notification + '<b>' + appointment.appointment_datetime + '</b> <font color="#0000ff">' + appointment.doctor_name + '</font> ' + appointment.clinic_name + '\n'
+                visistshelve.close()
+        
+        if pushover_notification and notificationcounter > 0 :
+            if len(notification) > 1020 : notification = notification [0:960] + '<b><font color="#ff0000"> + more appointments online</font></b>'
+            if len(pushover_msgtitle) > 0 : pushover_msgtitle = pushover_msgtitle + ': '
+            client.send_message(notification, title=pushover_msgtitle + "Found " + str(notificationcounter) + " appointments", device=pushover_device,html=1)
+
         counter += 1
         time.sleep(interval*60)
 
