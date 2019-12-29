2d0a1dc27b715dadaf4b52275a8c76be0bfc68df
apqlzm <artur-czyzewski@protonmail.com>
2019-12-29 13:57:31 +0100

    black formatting

--- medicover_session.py
+++ medicover_session.py
@@ -9,9 +9,12 @@ from bs4 import BeautifulSoup
 
 from errors import IdsrvXsrfNotFound
 
-Appointment = namedtuple('Appointment', ['doctor_name', 'clinic_name', 'appointment_datetime'])
+Appointment = namedtuple(
+    "Appointment", ["doctor_name", "clinic_name", "appointment_datetime"]
+)
 
-class MedicoverSession():
+
+class MedicoverSession:
     """
         Creating (log_in) and killing (log_out) session.
     """
@@ -21,12 +24,12 @@ class MedicoverSession():
         self.password = password
         self.session = requests.Session()
         self.headers = {
-            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0',
-            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-            'Accept-Language': 'pl,en-US,en;q=0.5',
-            'Accept-Encoding': 'gzip, deflate, br',
-            'Connection': 'keep-alive',
-            'Upgrade-Insecure-Requests': '1'
+            "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+            "Accept-Language": "pl,en-US,en;q=0.5",
+            "Accept-Encoding": "gzip, deflate, br",
+            "Connection": "keep-alive",
+            "Upgrade-Insecure-Requests": "1",
         }
 
     def login_form_data(self, page_text):
@@ -34,19 +37,16 @@ class MedicoverSession():
             Helper function allowing to extract xsref hash and prepare data for login.
         """
 
-        data = {
-            'username': self.username,
-            'password': self.password
-        }
-        if 'idsrv.xsrf' in page_text:
-            for line in page_text.split('\n'):
-                if 'idsrv.xsrf' in line:
-                    line = line.replace('&quot;', '"')
-                    line = line.replace('</script>', '')
-                    line = re.sub(r'<script.*?>', '', line)
+        data = {"username": self.username, "password": self.password}
+        if "idsrv.xsrf" in page_text:
+            for line in page_text.split("\n"):
+                if "idsrv.xsrf" in line:
+                    line = line.replace("&quot;", '"')
+                    line = line.replace("</script>", "")
+                    line = re.sub(r"<script.*?>", "", line)
                     dzej_son = json.loads(line)
-                    xsrf = dzej_son['antiForgery']['value']
-                    data['idsrv.xsrf'] = xsrf
+                    xsrf = dzej_son["antiForgery"]["value"]
+                    data["idsrv.xsrf"] = xsrf
                     break
         else:
             raise IdsrvXsrfNotFound
@@ -56,9 +56,8 @@ class MedicoverSession():
         """
             Helper function allowing to extract oauth link from page content
         """
-        soup = BeautifulSoup(page_text, 'html.parser')
-        return soup.form['action']
-
+        soup = BeautifulSoup(page_text, "html.parser")
+        return soup.form["action"]
 
     def token_form_data(self, page_text):
         """
@@ -66,42 +65,47 @@ class MedicoverSession():
             page content
         """
         o = dict()
-        soup = BeautifulSoup(page_text, 'html.parser')
+        soup = BeautifulSoup(page_text, "html.parser")
         for descet in soup.form.descendants:
-            if descet.name == 'input':
-                if descet['name'] == 'code':
-                    o['code'] = descet['value']
-                elif descet['name'] == 'id_token':
-                    o['id_token'] = descet['value']
-                elif descet['name'] == 'scope':
-                    o['scope'] = descet['value']
-                elif descet['name'] == 'session_state':
-                    o['session_state'] = descet['value']
-                elif descet['name'] == 'state':
-                    o['state'] = descet['value']
+            if descet.name == "input":
+                if descet["name"] == "code":
+                    o["code"] = descet["value"]
+                elif descet["name"] == "id_token":
+                    o["id_token"] = descet["value"]
+                elif descet["name"] == "scope":
+                    o["scope"] = descet["value"]
+                elif descet["name"] == "session_state":
+                    o["session_state"] = descet["value"]
+                elif descet["name"] == "state":
+                    o["state"] = descet["value"]
         return o
 
     def log_in(self):
         """
             Login to Medicover website
         """
-        #TODO: this method needs to be cleaned
+        # TODO: this method needs to be cleaned
 
         # 1. GET https://mol.medicover.pl/Users/Account/LogOn?ReturnUrl=%2F
-        response = self.session.get('https://mol.medicover.pl/Users/Account/LogOn?ReturnUrl=%2F',
-                                    headers=self.headers, allow_redirects=False)
-        next_url = response.headers['Location']
+        response = self.session.get(
+            "https://mol.medicover.pl/Users/Account/LogOn?ReturnUrl=%2F",
+            headers=self.headers,
+            allow_redirects=False,
+        )
+        next_url = response.headers["Location"]
 
         # 2. GET
         # https://oauth.medicover.pl/connect/authorize?client_id=Mcov_Mol&response_type=code+id_token&scope=openid&redirect_uri=https%3A%2F%2Fmol...
         response = self.session.get(
-            next_url, headers=self.headers, allow_redirects=False)
-        next_url = response.headers['Location']
+            next_url, headers=self.headers, allow_redirects=False
+        )
+        next_url = response.headers["Location"]
 
         # 3. GET
         # https://oauth.medicover.pl/login?signin=5512f89689e74ce9d5515f6a84d2b176
         response = self.session.get(
-            next_url, headers=self.headers, allow_redirects=False)
+            next_url, headers=self.headers, allow_redirects=False
+        )
         login_url = response.url
         data = self.login_form_data(response.text)
 
@@ -109,130 +113,148 @@ class MedicoverSession():
         # https://oauth.medicover.pl/login?signin=5512f89689e74ce9d5515f6a84d2b176
         next_url = login_url
         self.session.headers.update(
-            {'Content-Type': 'application/x-www-form-urlencoded'})
-        self.session.headers.update({'Origin': 'https://oauth.medicover.pl'})
-        self.session.headers.update({'Referer': login_url})
+            {"Content-Type": "application/x-www-form-urlencoded"}
+        )
+        self.session.headers.update({"Origin": "https://oauth.medicover.pl"})
+        self.session.headers.update({"Referer": login_url})
         response = self.session.post(
-            next_url, headers=self.headers, data=data, allow_redirects=False)
+            next_url, headers=self.headers, data=data, allow_redirects=False
+        )
 
         # 5. GET
         # https://oauth.medicover.pl/connect/authorize?client_id=Mcov_Mol&response_type=code%20id_token&scope=openid&redirect_uri=h...
-        next_url = response.headers['Location']
+        next_url = response.headers["Location"]
         response = self.session.get(
-            next_url, headers=self.headers, allow_redirects=False)
-        self.session.headers.update({'Referer': next_url})
+            next_url, headers=self.headers, allow_redirects=False
+        )
+        self.session.headers.update({"Referer": next_url})
         next_url = self.oauth_sign_in(response.text)
         data = self.token_form_data(response.text)
         self.session.headers.update(
-            {'Content-Type': 'application/x-www-form-urlencoded'})
+            {"Content-Type": "application/x-www-form-urlencoded"}
+        )
 
         # 6. POST
         # https://mol.medicover.pl/Medicover.OpenIdConnectAuthentication/Account/OAuthSignIn
         response = self.session.post(
-            next_url, headers=self.headers, data=data, allow_redirects=False)
-        self.session.headers.pop('Content-Type')
-        next_url = 'https://mol.medicover.pl/'
-        self.session.headers.update({'Referer': login_url})
+            next_url, headers=self.headers, data=data, allow_redirects=False
+        )
+        self.session.headers.pop("Content-Type")
+        next_url = "https://mol.medicover.pl/"
+        self.session.headers.update({"Referer": login_url})
 
         # 7. GET https://mol.medicover.pl/
         response = self.session.get(
-            next_url, headers=self.headers, data=data, allow_redirects=False)
+            next_url, headers=self.headers, data=data, allow_redirects=False
+        )
 
         # 8. GET https://mol.medicover.pl/
         response = self.session.get(
-            next_url, headers=self.headers, data=data, allow_redirects=False)
+            next_url, headers=self.headers, data=data, allow_redirects=False
+        )
         return response
 
     def _parse_search_results(self, result):
         """
         take search results in json format end transporm it to list of namedtuples
         """
-    
+
         result = result.json()
-        result = result['items']
+        result = result["items"]
         appointments = []
 
         for r in result:
             appointment = Appointment(
-                doctor_name=r['doctorName'], 
-                clinic_name=r['clinicName'], 
-                appointment_datetime=r['appointmentDate']
+                doctor_name=r["doctorName"],
+                clinic_name=r["clinicName"],
+                appointment_datetime=r["appointmentDate"],
             )
             appointments.append(appointment)
 
-        return appointments        
+        return appointments
 
     def search_appointments(self, *args, **kwargs):
 
-        if not ('clinic' in kwargs
-                and 'region' in kwargs
-                and 'start_date' in kwargs
-                and 'bookingtype' in kwargs
-                and 'specialization' in kwargs
-                and 'doctor' in kwargs):
+        if not (
+            "clinic" in kwargs
+            and "region" in kwargs
+            and "start_date" in kwargs
+            and "bookingtype" in kwargs
+            and "specialization" in kwargs
+            and "doctor" in kwargs
+        ):
             return
-        
 
-        BASE_URL = 'mol.medicover.pl'
+        BASE_URL = "mol.medicover.pl"
         headers = self.session.headers
-        headers.update({
-            'Host': BASE_URL,
-            'Accept': '*/*',
-            'Accept-Encoding': 'gzip, deflate, br',
-            'Accept-Language': 'en-US,en;q=0.5',
-            'Content-Type': 'application/json',
-            'X-Requested-With': 'XMLHttpRequest',
-            'Referer': 'https://mol.medicover.pl/MyVisits'
-            })
-    
+        headers.update(
+            {
+                "Host": BASE_URL,
+                "Accept": "*/*",
+                "Accept-Encoding": "gzip, deflate, br",
+                "Accept-Language": "en-US,en;q=0.5",
+                "Content-Type": "application/json",
+                "X-Requested-With": "XMLHttpRequest",
+                "Referer": "https://mol.medicover.pl/MyVisits",
+            }
+        )
 
-        region_id = int(kwargs['region'])
-        service_ids = [int(kwargs['specialization'])] if kwargs['bookingtype'] == 2 else [
-            int(kwargs['service'])]
-        clinic_ids = [kwargs['clinic']] if kwargs['clinic'] > 0 else []
-        doctor_ids = [kwargs['doctor']] if kwargs['doctor'] > 0 else []
+        region_id = int(kwargs["region"])
+        service_ids = (
+            [int(kwargs["specialization"])]
+            if kwargs["bookingtype"] == 2
+            else [int(kwargs["service"])]
+        )
+        clinic_ids = [kwargs["clinic"]] if kwargs["clinic"] > 0 else []
+        doctor_ids = [kwargs["doctor"]] if kwargs["doctor"] > 0 else []
 
         search_params = {
             "regionIds": [region_id],
-            "serviceTypeId": kwargs['bookingtype'],
+            "serviceTypeId": kwargs["bookingtype"],
             "serviceIds": service_ids,
             "clinicIds": clinic_ids,
             "doctorLanguagesIds": [],
             "doctorIds": doctor_ids,
-            "searchSince": kwargs['start_date'],
-            "startTime": kwargs['start_time'],
-            "endTime": kwargs['end_time']
+            "searchSince": kwargs["start_date"],
+            "startTime": kwargs["start_time"],
+            "endTime": kwargs["end_time"],
         }
-   
+
         result = self.session.post(
-            f'https://{BASE_URL}/api/MyVisits/SearchFreeSlotsToBook', 
-            json=search_params, 
-            params={'language': 'pl-PL'},
-            headers=headers
+            f"https://{BASE_URL}/api/MyVisits/SearchFreeSlotsToBook",
+            json=search_params,
+            params={"language": "pl-PL"},
+            headers=headers,
         )
 
         appointments = self._parse_search_results(result)
 
-        if kwargs.get('end_date') is not None:
-            def is_appointment_before_date(appointment: Appointment, date: datetime) -> bool:
+        if kwargs.get("end_date") is not None:
+
+            def is_appointment_before_date(
+                appointment: Appointment, date: datetime
+            ) -> bool:
                 def parse_appointment_datetime_to_datetime(iso_date: str) -> datetime:
                     return datetime.strptime(iso_date, "%Y-%m-%dT%H:%M:%S")
 
-                def get_end_of_day(dt: datetime) -> datetime: return dt + timedelta(days=1, microseconds=-1)
+                def get_end_of_day(dt: datetime) -> datetime:
+                    return dt + timedelta(days=1, microseconds=-1)
 
-                return parse_appointment_datetime_to_datetime(appointment.appointment_datetime) <= get_end_of_day(date)
+                return parse_appointment_datetime_to_datetime(
+                    appointment.appointment_datetime
+                ) <= get_end_of_day(date)
 
-            end_date = datetime.strptime(kwargs['end_date'], "%Y-%m-%d")
-            appointments = [a for a in appointments if is_appointment_before_date(a, end_date)]
+            end_date = datetime.strptime(kwargs["end_date"], "%Y-%m-%d")
+            appointments = [
+                a for a in appointments if is_appointment_before_date(a, end_date)
+            ]
 
         return appointments
 
     def load_search_form(self):
         return self.session.get(
-            'https://mol.medicover.pl/MyVisits',
-            params={'bookingTypeId': 2,
-                    'mex': 'True',
-                    'pfm': 1}
+            "https://mol.medicover.pl/MyVisits",
+            params={"bookingTypeId": 2, "mex": "True", "pfm": 1},
         )
 
     def log_out(self):
@@ -240,20 +262,21 @@ class MedicoverSession():
             Logout from Medicover website
             Session lasts 10 minutes anyway
         """
-        next_url = 'https://mol.medicover.pl/Users/Account/LogOff'
-        self.headers.update({'Referer': 'https://mol.medicover.pl/'})
+        next_url = "https://mol.medicover.pl/Users/Account/LogOff"
+        self.headers.update({"Referer": "https://mol.medicover.pl/"})
         self.headers.update(self.session.headers)
-        #print(self.headers)
+        # print(self.headers)
         response = self.session.get(next_url, headers=self.headers)
         self.session.close()
         return response
 
+
 def load_available_search_params(field_name):
     FIELDS_NAMES = {
-        'specialization': 'availableSpecializations',
-        'region': 'availableRegions',
-        'clinic': 'availableClinics',
-        'doctor': 'availableDoctors'
+        "specialization": "availableSpecializations",
+        "region": "availableRegions",
+        "clinic": "availableClinics",
+        "doctor": "availableDoctors",
     }
 
     if field_name not in FIELDS_NAMES:
@@ -261,9 +284,9 @@ def load_available_search_params(field_name):
 
     field_name = FIELDS_NAMES[field_name]
 
-    params_file_content = ''
-    
-    params_path = os.path.join(os.path.dirname(__file__), 'ids/params.json')
+    params_file_content = ""
+
+    params_path = os.path.join(os.path.dirname(__file__), "ids/params.json")
 
     with open(params_path) as f:
         params_file_content = f.read()
--- medihunter.py
+++ medihunter.py
@@ -9,12 +9,15 @@ from typing import Callable, List
 
 import click
 
-from medicover_session import (Appointment, MedicoverSession,
-                               load_available_search_params)
+from medicover_session import (
+    Appointment,
+    MedicoverSession,
+    load_available_search_params,
+)
 from medihunter_notifiers import pushover_notify, telegram_notify
 
 now = datetime.now()
-now_formatted = now.strftime('%Y-%m-%dT02:00:00.000Z')
+now_formatted = now.strftime("%Y-%m-%dT02:00:00.000Z")
 
 
 def make_duplicate_checker() -> Callable[[Appointment], bool]:
@@ -37,88 +40,98 @@ def make_duplicate_checker() -> Callable[[Appointment], bool]:
 
 duplicate_checker = make_duplicate_checker()
 
+
 def notify_external_device(message: str, notifier: str, **kwargs):
     # TODO: add more notification providers
-    if notifier == 'pushover':
-        title = kwargs.get('notification_title')
+    if notifier == "pushover":
+        title = kwargs.get("notification_title")
         pushover_notify(message, title)
-    elif notifier == 'telegram':
+    elif notifier == "telegram":
         telegram_notify(message)
 
 
-def process_appointments(appointments: List[Appointment], iteration_counter: int, notifier: str, **kwargs):
+def process_appointments(
+    appointments: List[Appointment], iteration_counter: int, notifier: str, **kwargs
+):
 
     applen = len(appointments)
-    click.echo(click.style(
-        f'(iteration: {iteration_counter}) Found {applen} appointments', fg='green', blink=True))
-    
-    notification_message = ''
+    click.echo(
+        click.style(
+            f"(iteration: {iteration_counter}) Found {applen} appointments",
+            fg="green",
+            blink=True,
+        )
+    )
+
+    notification_message = ""
 
     for appointment in appointments:
         if duplicate_checker(appointment):
             click.echo(
-                appointment.appointment_datetime + ' ' +
-                click.style(appointment.doctor_name, fg='bright_green') + ' ' +
-                appointment.clinic_name
+                appointment.appointment_datetime
+                + " "
+                + click.style(appointment.doctor_name, fg="bright_green")
+                + " "
+                + appointment.clinic_name
             )
-            notification_message += f'{appointment.appointment_datetime} {appointment.doctor_name} {appointment.clinic_name}\n'
-    
+            notification_message += f"{appointment.appointment_datetime} {appointment.doctor_name} {appointment.clinic_name}\n"
+
     if notification_message:
-        notification_title = kwargs.get('notification_title')
-        notify_external_device(notification_message, notifier, notification_title=notification_title)
+        notification_title = kwargs.get("notification_title")
+        notify_external_device(
+            notification_message, notifier, notification_title=notification_title
+        )
 
 
 def validate_arguments(**kwargs) -> bool:
-    if kwargs['service'] == -1 and kwargs['bookingtype'] == 1:
-        click.echo(
-            'Service is required when bookingtype=1 (Diagnostic procedure)')
+    if kwargs["service"] == -1 and kwargs["bookingtype"] == 1:
+        click.echo("Service is required when bookingtype=1 (Diagnostic procedure)")
         return False
 
-    if kwargs['specialization'] == -1 and kwargs['bookingtype'] == 2:
-        click.echo(
-            'Specialization is required when bookingtype=2 (Consulting)')
+    if kwargs["specialization"] == -1 and kwargs["bookingtype"] == 2:
+        click.echo("Specialization is required when bookingtype=2 (Consulting)")
         return False
     return True
 
 
 @click.command()
-@click.option('--region', '-r', required=True, show_default=True)
-@click.option('--bookingtype', '-b', default=2, show_default=True)
-@click.option('--specialization', '-s', default=-1)
-@click.option('--clinic', '-c', default=-1)
-@click.option('--doctor', '-o', default=-1)
-@click.option('--start-date', '-d', default=now_formatted, show_default=True)
-@click.option('--end-date', '-f')
-@click.option('--start-time', '-a', default='0:00', show_default=True)
-@click.option('--end-time', '-g', default='23:59', show_default=True)
-@click.option('--service', '-e', default=-1)
-@click.option('--interval', '-i', default=0, show_default=True)
-@click.option('--days-ahead', '-j', default=1, show_default=True)
-@click.option('--enable-notifier', '-n', type=click.Choice(['pushover', 'telegram']))
-@click.option('--notification-title', '-t')
-@click.option('--user', prompt=True)
+@click.option("--region", "-r", required=True, show_default=True)
+@click.option("--bookingtype", "-b", default=2, show_default=True)
+@click.option("--specialization", "-s", default=-1)
+@click.option("--clinic", "-c", default=-1)
+@click.option("--doctor", "-o", default=-1)
+@click.option("--start-date", "-d", default=now_formatted, show_default=True)
+@click.option("--end-date", "-f")
+@click.option("--start-time", "-a", default="0:00", show_default=True)
+@click.option("--end-time", "-g", default="23:59", show_default=True)
+@click.option("--service", "-e", default=-1)
+@click.option("--interval", "-i", default=0, show_default=True)
+@click.option("--days-ahead", "-j", default=1, show_default=True)
+@click.option("--enable-notifier", "-n", type=click.Choice(["pushover", "telegram"]))
+@click.option("--notification-title", "-t")
+@click.option("--user", prompt=True)
 @click.password_option(confirmation_prompt=False)
-def find_appointment(user,
-                     password,
-                     region,
-                     bookingtype,
-                     specialization,
-                     clinic,
-                     doctor,
-                     start_date,
-                     end_date,
-                     start_time,
-                     end_time,
-                     service,
-                     interval,
-                     days_ahead,
-                     enable_notifier,
-                     notification_title):
-    
+def find_appointment(
+    user,
+    password,
+    region,
+    bookingtype,
+    specialization,
+    clinic,
+    doctor,
+    start_date,
+    end_date,
+    start_time,
+    end_time,
+    service,
+    interval,
+    days_ahead,
+    enable_notifier,
+    notification_title,
+):
+
     valid = validate_arguments(
-        bookingtype=bookingtype,
-        specialization=specialization,
-        service=service
+        bookingtype=bookingtype, specialization=specialization, service=service
     )
 
     if not valid:
@@ -130,10 +143,10 @@ def find_appointment(user,
     try:
         med_session.log_in()
     except Exception:
-        click.secho('Unsuccessful logging in', fg='red')
+        click.secho("Unsuccessful logging in", fg="red")
         return
 
-    click.echo('Logged in')
+    click.echo("Logged in")
 
     med_session.load_search_form()
 
@@ -142,48 +155,60 @@ def find_appointment(user,
         start_date_param = start_date
         for _ in range(days_ahead):
             found_appointments = med_session.search_appointments(
-            region=region, 
-            bookingtype=bookingtype,
-            specialization=specialization, 
-            clinic=clinic, 
-            doctor=doctor,
-            start_date=start_date_param,
-            end_date=end_date,
-            start_time=start_time,
-            end_time=end_time,
-            service=service)
+                region=region,
+                bookingtype=bookingtype,
+                specialization=specialization,
+                clinic=clinic,
+                doctor=doctor,
+                start_date=start_date_param,
+                end_date=end_date,
+                start_time=start_time,
+                end_time=end_time,
+                service=service,
+            )
 
             if not found_appointments:
                 break
 
             appointment_datetime = found_appointments[-1].appointment_datetime
-            appointment_datetime = datetime.strptime(appointment_datetime, "%Y-%m-%dT%H:%M:%S")
+            appointment_datetime = datetime.strptime(
+                appointment_datetime, "%Y-%m-%dT%H:%M:%S"
+            )
             appointment_datetime = appointment_datetime + timedelta(days=1)
             start_date_param = appointment_datetime.date().isoformat()
             appointments.extend(found_appointments)
 
         if not appointments:
-            click.echo(click.style(
-                f'(iteration: {iteration_counter}) No results found', fg='yellow'))
+            click.echo(
+                click.style(
+                    f"(iteration: {iteration_counter}) No results found", fg="yellow"
+                )
+            )
         else:
             process_appointments(
-                appointments, iteration_counter, notifier=enable_notifier, notification_title=notification_title)
+                appointments,
+                iteration_counter,
+                notifier=enable_notifier,
+                notification_title=notification_title,
+            )
 
         iteration_counter += 1
-        time.sleep(interval*60)
+        time.sleep(interval * 60)
 
 
-FIELD_NAMES = ['specialization', 'region', 'clinic', 'doctor']
+FIELD_NAMES = ["specialization", "region", "clinic", "doctor"]
 
 
 @click.command()
-@click.option('-f', '--field-name', type=click.Choice(FIELD_NAMES), default='specialization')
+@click.option(
+    "-f", "--field-name", type=click.Choice(FIELD_NAMES), default="specialization"
+)
 def show_params(field_name):
     params = load_available_search_params(field_name)
     for p in params:
-        text = p['text']
-        id_ = p['id']
-        print(f' {text} (id={id_})')
+        text = p["text"]
+        id_ = p["id"]
+        print(f" {text} (id={id_})")
 
 
 @click.group()
@@ -195,5 +220,5 @@ medihunter.add_command(show_params)
 medihunter.add_command(find_appointment)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     medihunter()
